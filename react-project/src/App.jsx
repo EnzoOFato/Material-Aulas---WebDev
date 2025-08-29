7522d7a77f9a1ba12a537069b0e25cf859616d31
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-28 22:41:48 -0300

    React

---
+++ react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+    <Header titulo= "André Silva" texto="O Nove é craque" cor="red"></Header>
+    <Header titulo="Ferrerinha" texto="10 gols final do ano" cor="white"></Header>
+    <Header titulo="Rafael" texto="O goleiro do tetra" cor="black"></Header>
+    <Header></Header>
+    </>
+  )
+}
+
+export default App
