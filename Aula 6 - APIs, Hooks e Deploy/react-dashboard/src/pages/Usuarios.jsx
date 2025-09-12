22b1160e0d96fe49318f991b4df4e33935c1788b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-11 22:42:00 -0300

    Hooks

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Usuarios() {
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => setUsuarios(data))
+    .catch(err => console.log(err))
+    .finally(console.log("Carregou desgraça")) 
+  },[])
+
+  const [usuarios, setUsuarios] = useState([])
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
+      {usuarios.map(pegaItem => (
+        <div key={pegaItem.id}>
+          <p>{pegaItem.name}</p>
+          <p>{pegaItem.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
